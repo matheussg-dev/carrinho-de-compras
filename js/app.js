bcba58b15638e7ce415c25fbdef0838f05cb2e1f
CAS489\matheus.gordilho <matheus.gordilho@castecnologia.com.br>
2024-01-16 14:20:44 -0300

    registrar produtos no carrinho final

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
 function adicionar() {
+
     //recuperar valores nome do produto, quantidade e valor
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
     let valorUnitario produto.split('R$ ')[1]; =
     let quantidade = document.getElementById('quantidade'); 
+
         //calcular o preço, o nosso subtotal
-    let preco = quantidade.value * valorUnitario;
-    let carrinho = document.getElementById('lista-produtos');
-    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
-    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R${preco}</span>
-  </section>`;
+        let preco = quantidade * valorUnitario;
+        let carrinho = document.getElementById('lista-produtos');
+        
+        //adicionar no carrinho
+            carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+        <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R${preco}</span>
+      </section>`;
 }
